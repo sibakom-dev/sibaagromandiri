112f1e498bb1c89cca1378f0b05028035716bebf
Ahmad Dhuha Habibullah <habibullahphys15@gmail.com>
2026-07-15 16:27:15 +0700

    Add .nojekyll to disable jekyll in github pages build

--- siba-agro/.eleventy.js
+++ siba-agro/.eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   // Passthrough copy for assets and admin folder
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy("src/.nojekyll");
 
   return {
     dir: {
